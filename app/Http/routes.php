06825377b011782491b8eb357764398b48512244
vagrant <vagrant@homestead>
2017-08-30 04:58:44 +0000

    fix helpers

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,9 +1,6 @@
 <?php
 
-Route::get('items', 'ItemController@index');
 Route::post('items/import', 'ItemController@import');
 Route::get('items/export', 'ItemController@export');
 
-Route::get('/', function () {
-    return view('subir');
-});
+Route::get('/', 'ItemController@index');
